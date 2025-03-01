c34ef6d19e18e8712dde8892611cfee771b15a18
Fisayo Rotibi <fisayorotibi@gmail.com>
2025-03-01 22:11:37 +0100

    Updated OfflineToast component: adjusted width and padding

--- src/components/ui/OfflineToast.tsx
+++ src/components/ui/OfflineToast.tsx
@@ -19,9 +19,9 @@ const OfflineToast: React.FC = () => {
   }, []);
 
   return (
-    <div className={`fixed top-0 left-1/2 transform -translate-x-1/2 mt-4 ml-6 p-4 transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'} bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-100 rounded-md flex items-center space-x-2`}> 
+    <div className={`fixed top-0 left-1/2 transform -translate-x-1/2 mt-4 ml-12 py-4 px-4 w-36 transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'} bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-100 rounded-md flex items-center space-x-2`}> 
       <AlertTriangle className="w-3 h-3" />
-      <span className="text-sm">You are offline.</span>
+      <span className="text-xs">You are offline.</span>
     </div>
   );
 };
